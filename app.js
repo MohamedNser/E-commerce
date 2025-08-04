f7a2098a567a8e1f3fead0996590c98b45a4592e
MohamedNaser <mohamdnaser283@gmail.com>
2025-08-04 06:19:29 +0300

    Added Morgan logger, asyncHandler, and custom DB method abstraction (find, findOne, update)

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ dotenv.config({ path: path.join(__dirname, "./config/.env") });
 import express from "express";
 import * as indexRouter from "./src/modules/index.router.js";
 import connectDB from "./DB/connection.js";
+import { globalErrorHandler } from "./src/services/errorHandling.js";
+import morgan from "morgan";
 const baseUrl = process.env.BASEURL
 const port = process.env.PORT;
 const app = express();
 //convert Buffer Data
 app.use(express.json());
+if (process.env.MOOD) {
+    app.use(morgan('dev'))
+} else {
+    app.use(morgan("combined"))
+}
+
 //Setup API Routing
 app.use(`${baseUrl}/auth`, indexRouter.authRouter);
 app.use(`${baseUrl}/user`, indexRouter.userRouter);
@@ -27,6 +35,8 @@ app.use(`${baseUrl}/brand`, indexRouter.branRouter);
 app.use("*", (req, res, next) => {
     res.send("In-valid Routing Plz check url  or  method");
 });
+//errorHandler
+app.use(globalErrorHandler)
 //connectionDB
 connectDB();
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/services/errorHandling.js
@@ -0,0 +1,16 @@
+export const asyncHandler = (fn)=>{
+    return (req,res,next)=>{
+        fn(req,res,next).catch(err =>{
+            next(new Error(err , {cause:500}))
+        })
+    }}
+
+    export const globalErrorHandler =(err,req,res,next)=>{
+    if (err) {
+        if (process.env.MOOD=== "DEV") {
+        res.status(err['cause'] || 500 ).json({message: err.message , stack: err.stack})
+        } else {
+        res.status(err['cause'] || 500 ).json({message: err.message})
+        }
+    }
+}
