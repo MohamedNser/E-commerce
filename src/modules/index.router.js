ecd03e495b1eb61465ce30adebbc2f518e0b7a94
MohamedNaser <mohamdnaser283@gmail.com>
2025-09-21 03:57:05 +0300

    fix node version in package.json

--- src/modules/index.router.js
+++ src/modules/index.router.js
@@ -13,36 +13,33 @@ import { globalErrorHandler } from "../services/errorHandling.js";
 import connectDB from "../../DB/connection.js";
 import morgan from "morgan";
 
-export const appRouter = (app)=>{
-
-
-
+export const appRouter = (app) => {
   //convert Buffer Data
-app.use(express.json());
-if (process.env.MOOD) {
-    app.use(morgan('dev'))
-} else {
-    app.use(morgan("combined"))
-}
-//baseUrl
-  const baseUrl = process.env.BASEURL
-//Setup API Routing
-app.use(`${baseUrl}/auth`, authRouter);
-app.use(`${baseUrl}/user`, userRouter);
-app.use(`${baseUrl}/product`, productRouter);
-app.use(`${baseUrl}/subCategory`, subcategoryRouter);
-app.use(`${baseUrl}/category`, categoryRouter);
-app.use(`${baseUrl}/reviews`, reviewsRouter);
-app.use(`${baseUrl}/coupon`, couponRouter);
-app.use(`${baseUrl}/cart`, cartRouter);
-app.use(`${baseUrl}/order`, orderRouter);
-app.use(`${baseUrl}/brand`, branRouter);
-//in-valid Page
-app.use("*", (req, res, next) => {
+  app.use(express.json());
+  if (process.env.MOOD) {
+    app.use(morgan("dev"));
+  } else {
+    app.use(morgan("combined"));
+  }
+  //baseUrl
+  const baseUrl = process.env.BASEURL;
+  //Setup API Routing
+  app.use(`${baseUrl}/auth`, authRouter);
+  app.use(`${baseUrl}/user`, userRouter);
+  app.use(`${baseUrl}/product`, productRouter);
+  app.use(`${baseUrl}/subCategory`, subcategoryRouter);
+  app.use(`${baseUrl}/category`, categoryRouter);
+  app.use(`${baseUrl}/reviews`, reviewsRouter);
+  app.use(`${baseUrl}/coupon`, couponRouter);
+  app.use(`${baseUrl}/cart`, cartRouter);
+  app.use(`${baseUrl}/order`, orderRouter);
+  app.use(`${baseUrl}/brand`, branRouter);
+  //in-valid Page
+  app.use("*", (req, res, next) => {
     res.send("In-valid Routing Plz check url  or method");
-});
-//errorHandler
-app.use(globalErrorHandler)
-//connectionDB
-connectDB();
-}
+  });
+  //errorHandler
+  app.use(globalErrorHandler);
+  //connectionDB
+  connectDB();
+};
